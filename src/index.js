ba2a960866a218e46b6a2ca00c3f793202aecad4
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2022-10-05 19:47:36 -0500

    import web3modal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Web3Modal } from "@web3modal/react";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const config = {
+  projectId: "",
+  theme: "dark",
+  accentColor: "default",
+  ethereum: {
+    appName: "web3Modal",
+  },
+};
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <App />
+    <Web3Modal config={config} />
+  </>
+);
